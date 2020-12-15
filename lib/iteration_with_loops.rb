21c18914f783b2c4015b0d7bf361cad626e2f65b
Jeffrey Platt <jsplatt@gmail.com>
2020-12-15 15:49:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,8 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   string_elements = ""
-  while element_index < scr[row_index].count do
-    if scr[row_index][element_index]
+  while element_index < src[row_index].count do
+    if src[row_index][element_index]
       array << 
 
 
